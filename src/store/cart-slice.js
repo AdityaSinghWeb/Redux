75ad6cf0da4b88bc8686397313b791a87125c4ce
AdityaSingh-web18 <adityasingh180710@gmail.com>
2024-09-27 21:59:38 +0530

    1st update

---
+++ src/store/cart-slice.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItems = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+      if (!existingItems) {
+        state.items.push({
+          id: newItem.id,
+          quantity: 1,
+          price: newItem.price,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItems.totalPrice = existingItems.totalPrice + newItem.price;
+        existingItems.quantity++;
+      }
+    },
+
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItems = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItems.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItems.quantity--;
+        existingItems.totalPrice =
+          existingItems.totalPrice - existingItems.price;
+      }
+    },
+  },
+});
+
+export const cartAction = cartSlice.actions;
+
+export default cartSlice;
